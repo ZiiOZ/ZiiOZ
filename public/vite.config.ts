dd28cc37e482ecf19e263fd7912aae4e9ad36636
ZiiOZ <support@ziioz.com>
2025-05-04 09:35:40 +1000

    fix: update vite config to resolve blank screen issue

--- public/vite.config.ts
+++ public/vite.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  root: '.', // this points to your project root (where index.html is in public/)
   plugins: [react()],
   build: {
-    outDir: 'dist'
-  }
-});
+    outDir: 'dist',
+    rollupOptions: {
+      input: './public/index.html',
+    },
+  },
+})
