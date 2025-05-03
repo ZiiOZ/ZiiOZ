b62daddb132d387e2a57b8d30f895f6c6f5ac9c9
ZiiOZ <support@ziioz.com>
2025-05-03 16:39:57 +1000

    Fixed JSX + routing issues

--- public/src/App.tsx
+++ public/src/App.tsx
@@ -1,12 +1,12 @@
-import { Route, Switch } from 'wouter';
+import { Route } from 'wouter';
 import Home from './pages/Home';
 import SimpleCheckout from './pages/SimpleCheckout';
 
 export default function App() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/checkout" component={SimpleCheckout} />
-    </Switch>
+    <>
+      <Route path="/" component={() => <Home />} />
+      <Route path="/checkout" component={() => <SimpleCheckout />} />
+    </>
   );
 }
