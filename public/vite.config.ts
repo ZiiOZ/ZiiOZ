00bd19050996219cf6e8772c195ca978a4c3d103
ZiiOZ <support@ziioz.com>
2025-05-05 12:32:32 +1000

    Fix Vite root & index.html for deployment

--- public/vite.config.ts
+++ public/vite.config.ts
@@ -1,20 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // root is the project directory
-  base: './', // important for relative paths
+  root: '.', // Make sure root is current directory
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: './index.html',
-    },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
   },
 });
