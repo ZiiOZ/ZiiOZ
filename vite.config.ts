af0b4611448b4a94cfc111541dd4602f732e0089
ZiiOZ <support@ziioz.com>
2025-05-06 13:29:55 +1000

    Fix Vite build config to resolve entry index.html

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.', // your project root (index.html is here)
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html', // tell Vite what the entry point is
+    },
+  },
 })
