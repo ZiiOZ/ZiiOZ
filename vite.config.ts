279fd06638981d3a9086fd23b8fb3f0f1768a0f2
ZiiOZ <support@ziioz.com>
2025-05-06 13:10:22 +1000

    Cleaned Vite config: reverted to default entry behavior

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // start from root
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: 'src/index.html', // <-- corrected path
-    },
-  },
 })
